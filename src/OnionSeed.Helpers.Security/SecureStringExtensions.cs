793d024715f2fdb0cc997c6cfe822f95c584de0b
RJ Hollberg <rj.hollberg@outlook.com>
2019-05-09 16:41:45 -0600

    Add extension method to be able to easily append strings to SecureString.

---
+++ src/OnionSeed.Helpers.Security/SecureStringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace OnionSeed.Helpers.Security
+{
+	/// <summary>
+	/// Contains extension methods relating to the <see cref="SecureString"/> data type.
+	/// </summary>
+	public static class SecureStringExtensions
+	{
+		/// <summary>
+		/// Appends all the specified characters to the end of the given <see cref="SecureString"/>.
+		/// </summary>
+		/// <param name="secureString">The <see cref="SecureString"/> to which the characters should be appended.</param>
+		/// <param name="chars">The list of characters to append to the given <see cref="SecureString"/>.</param>
+		/// <exception cref="ObjectDisposedException">This <see cref="SecureString"/> has already been disposed.</exception>
+		/// <exception cref="InvalidOperationException">This <see cref="SecureString"/> is read-only.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Performing this operation would make the length of this <see cref="SecureString"/> greater than 65,536 characters.</exception>
+		/// <exception cref="CryptographicException">An error occurred while protecting or unprotecting the value of this <see cref="SecureString"/>.</exception>
+		public static void Append(this SecureString secureString, IEnumerable<char> chars)
+		{
+			if (secureString == null)
+				throw new ArgumentNullException(nameof(secureString));
+
+			if (chars != null)
+			{
+				foreach (var c in chars)
+				{
+					secureString.AppendChar(c);
+				}
+			}
+		}
+	}
+}
---
+++ test/OnionSeed.Helpers.Security.Tests/SecureStringExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Security;
+using FluentAssertions;
+using Xunit;
+
+namespace OnionSeed.Helpers.Security
+{
+	public class SecureStringExtensionsTests
+	{
+		[Fact]
+		public void Append_ShouldThrowException_WhenSecureStringIsNull()
+		{
+			// Arrange
+			SecureString secureData = null;
+
+			// Act
+			Action action = () => secureData.Append("bob");
+
+			// Assert
+			action.Should().Throw<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void Append_ShouldDoNothing_WhenListIsNull()
+		{
+			// Arrange
+			using (var secureData = new SecureString())
+			{
+				// Act
+				secureData.Append(null);
+
+				// Assert
+				secureData.Length.Should().Be(0);
+			}
+		}
+
+		[Fact]
+		public void Append_ShouldDoNothing_WhenListIsEmpty()
+		{
+			// Arrange
+			using (var secureData = new SecureString())
+			{
+				// Act
+				secureData.Append(Enumerable.Empty<char>());
+
+				// Assert
+				secureData.Length.Should().Be(0);
+			}
+		}
+
+		[Fact]
+		public void Append_ShouldAppendData()
+		{
+			// Arrange
+			const string data = "Bob's unicode data: №Ω";
+			using (var secureData = new SecureString())
+			{
+				// Act
+				secureData.Append(data);
+
+				// Assert
+				secureData.Length.Should().Be(data.Length);
+			}
+		}
+	}
+}
