958f8c08535f3e3f235b408a03453ae05c48e88d
RJ Hollberg <rj.hollberg@outlook.com>
2019-05-09 13:54:47 -0600

    Add extension method to execute an Action in a Constrained Execution Environment. Basic tests are included, but ... honestly I'm just following M$ docs. I don't know how to test this. :|

---
+++ src/OnionSeed.Helpers.Security/ActionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace OnionSeed.Helpers.Security
+{
+	/// <summary>
+	/// Contains extension methods for the <see cref="Action"/> data types.
+	/// </summary>
+	public static class ActionExtensions
+	{
+		/// <summary>
+		/// Executes the given <see cref="Action"/> in a <a href="https://docs.microsoft.com/en-us/dotnet/framework/performance/constrained-execution-regions">Constrained Execution Region</a> in memory.
+		/// </summary>
+		/// <param name="action">The <see cref="Action"/> to be executed.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="action"/> is <c>null</c>.</exception>
+		public static void ExecuteInConstrainedRegion(this Action action)
+		{
+			if (action == null)
+				throw new ArgumentNullException(nameof(action));
+
+			RuntimeHelpers.PrepareConstrainedRegions();
+			try
+			{
+			}
+			finally
+			{
+				action();
+			}
+		}
+	}
+}
---
+++ test/OnionSeed.Helpers.Security.Tests/ActionExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace OnionSeed.Helpers.Security
+{
+	public class ActionExtensionsTests
+	{
+		[Fact]
+		public void ExecuteInConstrainedRegion_ShouldThrowException_WhenActionIsNull()
+		{
+			// Arrange
+			Action subject = null;
+
+			// Act
+			Action action = () => subject.ExecuteInConstrainedRegion();
+
+			// Assert
+			action.Should().Throw<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void ExecuteInConstrainedRegion_ShouldInvokeAction_WhenActionIsGiven()
+		{
+			// Arrange
+			var result = false;
+			Action action = () => result = true;
+
+			// Act
+			action.ExecuteInConstrainedRegion();
+
+			// Assert
+			result.Should().BeTrue();
+		}
+	}
+}
